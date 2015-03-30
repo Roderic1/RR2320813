81aa1ed764d6af39842681b994b288c4ffdcd376
roderic1 <rrodriguez171@student.rcc.edu>
2015-03-30 13:30:09 -0700

    Signed-off-by: roderic1 <rrodriguez171@student.rcc.edu>

---
+++ CppApplication_2/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 30, 2015, 1:15 PM
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+
+using namespace std;
+
+//User Libraries
+#include "student.h"
+
+//Global
+
+//Function prototypes
+
+
+//Start execution here
+int main(int argc, char** argv) {
+    //
+
+    return 0;
+}
+
